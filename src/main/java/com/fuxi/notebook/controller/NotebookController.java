384607b50c4bedc0dbe3f991aa2c629876b1eb0e
wangsheng219 <wangsheng219@163.com>
2020-11-10 10:48:25 +0800

    first commit

---
+++ src/main/java/com/fuxi/notebook/controller/NotebookController.java
@@ -0,0 +1,25 @@
+package  com.fuxi.notebook.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/")
+public class NotebookController{
+
+	@GetMapping("list")
+	public  String showUpdateForm(Model model){
+		return "index";
+	}
+
+}
